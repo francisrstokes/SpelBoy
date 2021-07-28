2193dd64f09d291e7a2ffd64aab5cd2896ba513e
Frank Stokes <frank@wearereasonablepeople.com>
2021-07-28 19:52:01 +0200

    Remove parcel and add webpack

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: './src/spelboy.ts',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Spelboy',
+      template: 'src/index.html'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: [
+          /tests/,
+          /node_modules/
+        ],
+        include: path.resolve(__dirname, "src"),
+      },
+      {
+        test: /\.(bin|gb)$/,
+        use: 'arraybuffer-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 9000,
+  },
+};
